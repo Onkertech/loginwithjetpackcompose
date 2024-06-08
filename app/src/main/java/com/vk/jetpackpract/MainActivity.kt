beb39e555118d723f0ef4576e2cce84a9502b3ce
Vishwnath <vkdev021@gmail.com>
2024-06-08 10:33:55 +0530

    second commit

--- app/src/main/java/com/vk/jetpackpract/MainActivity.kt
+++ app/src/main/java/com/vk/jetpackpract/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //change 1
            LoginScreen()
         }
     }
